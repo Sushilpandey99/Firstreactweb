e262682af0328133ee65b5822c9eead364a2af14
Sushil Pandey <susilpanday9@gmail.com>
2024-07-04 13:39:27 +0545

    second commit

---
+++ secondproject/src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css" 
+import pixels from './assets/pixels.jpg'
+function App() {
+return(<>
+<img className="p-1" src={pixels} alt="this is fun" />
+  <h1>Sushil Pandey</h1>
+  <p>Iam from damak-1 jhapa</p>
+</>
+);
+}
+
+
+export default App
